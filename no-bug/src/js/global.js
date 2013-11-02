36574e4a7a9a87a28beb95f123739bff1ee05498
dev-jan <Jan@JAN-PC>
2013-11-02 13:28:12 +0100

    html: administration -> User/Group designed

---
+++ no-bug/src/js/global.js
@@ -0,0 +1,12 @@
+
+function hideshow(which){
+	if (!document.getElementById) {
+		return;
+	}
+	if (which.style.display=="block") {
+		which.style.display="none";
+	}
+	else {
+		which.style.display="block";
+	}
+}
