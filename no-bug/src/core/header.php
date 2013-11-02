912e4a41e026707134f5e6009643b318952ada2d
randombenj <ibenjfa@gmail.com>
2013-11-01 17:59:31 -0700

    Added html from index.html to header.php

--- no-bug/src/core/header.php
+++ no-bug/src/core/header.php
@@ -1,8 +1,35 @@
 <html>
 	<head>
 		<title>no-bug | pre alpha 0.0.1</title>
-		<script type="text/javascript" src="//raw.github.com/less/less.js/master/dist/less-1.5.0.min.js" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="author" content="Benj Fassbind & Jan Bucher" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="style/bootstrap.min.css" />
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//raw.github.com/less/less.js/master/dist/less-1.5.0.min.js" ></script>
 	</head>
 	<body>
-	<?php
+	
+	<!-- The Main navigation Head. -->
+	<div id="header" class="navbar navbar-default navbar-fixed-top">
+		<a class="navbar-brand" href="#">no-bug</a>
+		<ul class="nav navbar-nav">
+      		<li class="active"><a href="#">Main</a></li>
+      		<li class=""><a href="#">Projects</a></li>
+      		<li class=""><a href="#">Administration</a></li>
+      	</ul>
+		
+		 <ul class="nav navbar-nav navbar-right">
+	      	<li class="dropdown">
+	        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hans Vader <b class="caret"></b></a>
+		        <ul class="dropdown-menu">
+		        	<li><a href="#">Action</a></li>
+		        	<li><a href="#">Another action</a></li>
+		        	<li><a href="#">Something else here</a></li>
+		        	<li class="divider"></li>
+		        	<li><a href="#">Separated link</a></li>
+		        </ul>
+	      	</li>
+	    </ul>
+	</div>
 		
--- no-bug/src/index.php
+++ no-bug/src/index.php
@@ -1,5 +1,10 @@
 <?php
-	include 'core/header.php';
+	include 'core/header.php';	
+?>
+	<div class="main">
+		<?php echo "No-bug Plattform funktioniert"; ?>
+	</div>
+	
+<?php 
 	include 'core/footer.php';
-	echo "No-bug Plattform funktioniert";
 ?>
