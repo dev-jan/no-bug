67579ccea625e9edf71d1f3beebe67e09600cc38
Benj Fassbind <ibenjfa@gmail.com>
2013-11-03 13:57:48 +0100

    Minor design changes (max page width)

--- no-bug/src/core/header.php
+++ no-bug/src/core/header.php
@@ -15,33 +15,35 @@ if (!isset($_SESSION['userId'])) {
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<meta name="author" content="Benj Fassbind & Jan Bucher" />
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<link rel="stylesheet" href="style/bootstrap.min.css" />
-		<link rel="stylesheet/less" type="text/css" href="style/global.less" />
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="js/less.js" ></script>
+		<link rel="stylesheet" href="<?php echo ROOTPATH; ?>style/bootstrap.min.css" />
+		<link rel="stylesheet/less" type="text/css" href="<?php echo ROOTPATH; ?>style/global.less" />
+		<script type="text/javascript" src="<?php echo ROOTPATH; ?>js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<?php echo ROOTPATH; ?>js/less.js" ></script>
 	</head>
 	<body>
 	
 	<!-- The Main navigation Head. -->
 	<div id="header" class="navbar navbar-default navbar-fixed-top">
-		<a class="navbar-brand" href="#">no-bug</a>
-		<ul class="nav navbar-nav">
-      		<li class="active"><a href="#">Main</a></li>
-      		<li class=""><a href="#">Projects</a></li>
-      		<li class=""><a href="#">Administration</a></li>
-      	</ul>
-		
-		 <ul class="nav navbar-nav navbar-right">
-	      	<li class="dropdown">
-	        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hans Vader <b class="caret"></b></a>
-		        <ul class="dropdown-menu">
-		        	<li><a href="#">Action</a></li>
-		        	<li><a href="#">Another action</a></li>
-		        	<li><a href="#">Something else here</a></li>
-		        	<li class="divider"></li>
-		        	<li><a href="#">Separated link</a></li>
-		        </ul>
-	      	</li>
-	    </ul>
+		<div id="header-wrapper">
+			<a class="navbar-brand" href="#">no-bug</a>
+			<ul class="nav navbar-nav">
+	      		<li class="active"><a href="#">Main</a></li>
+	      		<li class=""><a href="#">Projects</a></li>
+	      		<li class=""><a href="#">Administration</a></li>
+	      	</ul>
+			
+			 <ul class="nav navbar-nav navbar-right">
+		      	<li class="dropdown">
+		        	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hans Vader <b class="caret"></b></a>
+			        <ul class="dropdown-menu">
+			        	<li><a href="#">Action</a></li>
+			        	<li><a href="#">Another action</a></li>
+			        	<li><a href="#">Something else here</a></li>
+			        	<li class="divider"></li>
+			        	<li><a href="#">Separated link</a></li>
+			        </ul>
+		      	</li>
+		    </ul>
+	    </div>
 	</div>
 		
